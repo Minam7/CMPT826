88ec1fda1f07fe8174353bd49681295753e71a78
Mina Moosavi <sem311@usask.ca>
2020-02-02 19:58:06 -0600

    add HW1 filtering

--- HW1/data_gathering.py
+++ HW1/data_gathering.py
@@ -5,9 +5,13 @@ engine = db.create_engine('mysql://sem311:p^HA89/h@crepe.usask.ca:3306/SHED10')
 
 connection = engine.connect()
 metadata = db.MetaData()
-battery = db.Table('battery', metadata, autoload=True, autoload_with=engine)
 
-# Equivalent to 'SELECT * FROM battery'
+'''
+
+Obtaining battery table
+
+# Equivalent to 'SELECT * FROM battery
+battery = db.Table('battery', metadata, autoload=True, autoload_with=engine)
 query = db.select([battery])
 
 # getting data by executing the query above
@@ -23,3 +27,30 @@ battery_data = battery_data.drop('index', 1)
 
 # saving data frame as pickle object for future use
 battery_data.to_pickle('data/battery.pkl')
+
+print('fetching battery data completed')
+
+'''
+
+'''
+Obtaining gps table
+'''
+# Equivalent to 'SELECT * FROM gps
+gps = db.Table('gps', metadata, autoload=True, autoload_with=engine)
+query = db.select([gps])
+
+# getting data by executing the query above
+GPSResultProxy = connection.execute(query)
+GPSResultSet = GPSResultProxy.fetchall()
+
+# converting data to data frame
+gps_data = pd.DataFrame(GPSResultSet)
+gps_data.columns = GPSResultSet[0].keys()
+
+# removing index column from data - 0 for rows and 1 for column
+gps_data = gps_data.drop('index', 1)
+
+# saving data frame as pickle object for future use
+gps_data.to_pickle('data/gps.pkl')
+
+print('fetching gps data completed')
--- HW1/filtering.py
+++ HW1/filtering.py
@@ -1,21 +1,23 @@
 import pandas as pd
 
-# reading data from pickle object
-battery_data = pd.read_pickle('data/battery.pkl')
-
-############################################################
 '''
 Part A: removing users with 50% and 75% battery records
 '''
+
+# reading data from pickle object
+battery_data = pd.read_pickle('data/battery.pkl')
+
 # counting number of battery information per user
 battery_info = battery_data.groupby(['user_id']).size().reset_index(name='record_count')
 
 # finding number of users before filtering
 all_users, _ = battery_info.shape
 print('There is a total of', all_users, 'users.')
+
 '''
 50%
 '''
+
 # calculating filtering cutoff
 cutoff_percentage = 0.5
 max_battery_info = (60 / 5) * 24 * 30
@@ -29,9 +31,11 @@ users_filter_50, _ = battery_info_50.shape
 battery_data_50 = pd.merge(left=battery_data, right=battery_info_50, left_on='user_id', right_on='user_id')
 
 print(all_users - users_filter_50, 'users are removed in 50% threshold.')
+
 '''
 75%
 '''
+
 # calculating filtering cutoff
 cutoff_percentage = 0.75
 battery_cutoff = cutoff_percentage * max_battery_info
@@ -45,7 +49,72 @@ battery_data_75 = pd.merge(left=battery_data, right=battery_info_75, left_on='us
 
 print(all_users - users_filter_75, 'users are removed in 75% threshold.')
 
+# saving data frame as pickle object for future use
+battery_data_50.to_pickle('data/battery_50.pkl')
+battery_data_75.to_pickle('data/battery_75.pkl')
+
 ############################################################
+
 '''
 Part B: removing GPS data outside of Saskatoon
 '''
+
+# reading data from pickle object
+gps_data = pd.read_pickle('data/gps.pkl')
+
+# finding number of all records
+records_num, _ = gps_data.shape
+print('There is a total of', records_num, 'GPS record.')
+
+'''
+Accuracy filtering
+'''
+
+# filtering accuracy more than 100
+gps_data_filter_acc = gps_data.loc[gps_data['accu'] < 100]
+gps_filter_accu, _ = gps_data_filter_acc.shape
+
+print(records_num - gps_filter_accu, 'records are removed for accuracies more than 100m.')
+
+'''
+Latitude filtering
+'''
+
+# outside latitude range
+gps_data_filter_lat = gps_data.loc[gps_data['lat'] > 52.058366]
+gps_filter_lat_low, _ = gps_data_filter_lat.shape
+
+print(records_num - gps_filter_lat_low, 'records are removed for latitudes less than 52.058367')
+
+gps_data_filter_lat = gps_data_filter_lat.loc[gps_data_filter_lat['lat'] < 52.214609]
+gps_filter_lat_high, _ = gps_data_filter_lat.shape
+
+print(records_num - gps_filter_lat_high - gps_filter_lat_low, 'records are removed for latitudes more than 52.214608')
+
+'''
+Longitude filtering
+'''
+
+# outside longitude range
+gps_data_filter_lon = gps_data[gps_data['lon'] > -106.7649138128]
+gps_filter_lon_low, _ = gps_data_filter_lon.shape
+
+print(records_num - gps_filter_lon_low, 'records are removed for longitudes less -106.7649138128')
+
+gps_data_filter_lon = gps_data_filter_lon.loc[gps_data_filter_lon['lon'] < -106.52225319]
+gps_filter_lon_high, _ = gps_data_filter_lon.shape
+
+print(records_num - gps_filter_lon_high - gps_filter_lon_low,
+      'records are removed for latitudes more than -106.52225318')
+
+############################################################
+# executing all filters to a final data frame
+gps_data = gps_data.loc[gps_data['accu'] < 100]
+gps_data = gps_data.loc[gps_data['lat'] > 52.058366]
+gps_data = gps_data.loc[gps_data['lat'] < 52.214609]
+gps_data = gps_data[gps_data['lon'] > -106.7649138128]
+gps_data = gps_data.loc[gps_data['lon'] < -106.52225319]
+
+records_num, _ = gps_data.shape
+
+print('GPS data size after filtering is ', records_num, 'records.')
